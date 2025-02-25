683ed82135073b393d85208d542c40c5f4aef0a6
atharva <atharvagupta2003@gmail.com>
2025-02-25 20:06:09 +1000

    changes

--- src/langgraph_mcp/retriever.py
+++ src/langgraph_mcp/retriever.py
@@ -39,10 +39,11 @@ def make_milvus_retriever(
     """Configure this agent to use milvus lite file based uri to store the vector index."""
     from langchain_milvus.vectorstores import Milvus
 
+    uri = os.environ["MILVUS_DB"]
     vstore = Milvus (
         embedding_function=embedding_model,
-        connection_args={"uri": os.environ["MILVUS_DB"]},
-        index_params={"metric_type": "L2", "index_type": "IVF_FLAT", "params": {}}
+        connection_args={"uri": uri},
+        index_params=None if uri.startswith("http") else {"index_type": "FLAT", "metric_type": "L2", "params": {}}
     )
     yield vstore.as_retriever()
 
